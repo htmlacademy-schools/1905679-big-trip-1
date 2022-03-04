f31aa0c6a387bdc58b07ea37270541156b2865e2
Dmitry <dimakorobitcyn@yandex.ru>
2022-03-04 13:00:44 +0500

    add site-menu in task-3

--- src/main.js
+++ src/main.js
@@ -5,12 +5,14 @@ import { createSiteSort } from './view/site-sort.js';
 import { createSiteEditForm } from './view/site-edit-form.js';
 import { createSiteFilters } from './view/site-filters.js';
 import { createSiteEventsList } from './view/site-event-list.js';
+import { createSiteMenu } from './view/site-menu.js';
 
 const siteNavigation = document.querySelector('.trip-controls__navigation');
 const siteEventsElement = document.querySelector('.trip-events');
 renderTemplate(siteEventsElement, createSiteEventsList(), RenderPosition.BEFOREEND);
 const siteEventsListElement = siteEventsElement.querySelector('.trip-events__list');
 const siteFilters = document.querySelector('.trip-controls__filters');
+const siteMenu = document.querySelector('.trip-main');
 
 const count = 3;
 
@@ -22,3 +24,4 @@ for (let i = 0; i < count; i++) {
 renderTemplate(siteEventsElement, createSiteSort(), RenderPosition.AFTERBEGIN);
 renderTemplate(siteEventsListElement, createSiteEditForm(), RenderPosition.AFTERBEGIN);
 renderTemplate(siteFilters, createSiteFilters(), RenderPosition.BEFOREEND);
+renderTemplate(siteMenu, createSiteMenu(), RenderPosition.AFTERBEGIN);
